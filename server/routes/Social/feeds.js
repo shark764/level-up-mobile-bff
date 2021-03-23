0117da844bd284c8def40b4a5d228c4cd9fa2bee
Federico Pineda <fedepimp@gmail.com>
2021-03-23 11:16:00 -0600

    get user friends id and added it to the query

--- server/routes/Social/feeds.js
+++ server/routes/Social/feeds.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const ObjectId = require('mongoose').Types.ObjectId
 const User = require('../../db/models/User')
+const User_Friends = require('../../db/models/User_Friends')
 const success = require('../../utils/helpers/response').success
 const error = require('../../utils/helpers/response').error
 const validateAccess = require('../../middlewares/validateAccess');
@@ -9,6 +10,7 @@ const router = express.Router()
 // Get friend's and group's posts for the user
 // GET /social/feed/
 router.get('/social/feed/', validateAccess, async (req, res) => {
+    const user_friends = await User_Friends.find({ userId: req.user.data._id }).distinct('friendUserId')
     try {        
         const feed = await User.aggregate([
             { $match: { "_id": ObjectId(req.user.data._id)} },
@@ -59,7 +61,7 @@ router.get('/social/feed/', validateAccess, async (req, res) => {
                     from: 'user_posts',
                     let: { groupId: '$groupId' },
                     pipeline: [
-                        { $match: {$expr: { $or: [ { $eq: ['$groupsId', "$$groupId"]} , { $in: [ '$userId', [ObjectId('6054d16693d8a4413c22827d')] ]  } ] }  } }
+                        { $match: {$expr: { $or: [ { $eq: ['$groupsId', "$$groupId"]} , { $in: [ '$userId', user_friends ]  } ] }  } }
                     ],
                     as: 'post'
                 }
@@ -107,95 +109,6 @@ router.get('/social/feed/', validateAccess, async (req, res) => {
             .status(400)
             .json(error({ requestId: req.id, code: 400, message: err }))
     }
-})
-
-// Update post by id
-// PATCH /social/post/60523db879c7cd3ab86342ce
-router.patch('/social/post/:id', validateAccess, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['urlMedia','caption', 'title']
-
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res
-            .status(400)
-            .json(error({ requestId: req.id, code: 400, message: 'Invalid updates!' }))
-    }
-
-    try {
-        const post = await User_Posts.findOne({_id: req.params.id, userId: req.user.data._id})
-    
-        if (!post) {
-            return res
-                .status(400)
-                .json(error({ requestId: req.id, code: 404 }))
-        }
-    
-        updates.forEach((update) => post[update] = req.body[update])
-        await post.save()
-        
-        return res
-            .status(201)
-            .json(success({ requestId: req.id, data: post }))
-    } catch (err) {
-        return res
-            .status(400)
-            .json(error({ requestId: req.id, code: 400, message: e }))
-    }
-
-})
-
-router.get('/social/feeds/', validateAccess, async (req, res) => {
-    try {
-        console.log(req.user.data._id)
-        const friendsPosts = await User_Friends.aggregate([{ 
-            $lookup: {
-                from: 'user_posts', 
-                localField: 'friendUserId', 
-                foreignField: 'userId', 
-                as: 'friend_posts'
-            }
-        },
-        {
-            $match: { 
-                userId: ObjectId(req.user.data._id),
-                friend_posts:  { $ne: []}
-            }            
-        },
-        {
-            $sort: {
-                "friend_posts.publishDate": -1
-            }
-        }])
-        // const friendsPosts = User_Friends.aggregate([{ 
-        //     $lookup: {
-        //         from: 'user_posts', 
-        //         localField: 'friendUserId', 
-        //         foreignField: 'userId', 
-        //         as: 'friend_posts'
-        //     }
-        // },
-        // {
-        //     $match: { 
-        //         userId: ObjectId('6053927de5fc283c3810c5af'),
-        //         friend_posts:  { $ne: []}
-        //     }            
-        // },
-        // {
-        //     $sort: {
-        //         "friend_posts.publishDate": 1
-        //     }
-        // }],
-        // function (err, data) {
-        //     res.json(data)
-        // })
-
-        res.json(friendsPosts)
-    } catch (err) {
-        console.log(err)
-    }
-    // get groups posts
 })
 
 module.exports = router

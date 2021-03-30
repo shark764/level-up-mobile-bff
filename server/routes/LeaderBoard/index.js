46d2a73130803c2c04d5d910b4b5f30bb4a30b72
Ferm9404 <Fernando.rivas@focusservices.com>
2021-03-30 00:30:44 -0600

    Adding rank handler for users that doesnt have matches and creating leaderboard endpoint

---
+++ server/routes/LeaderBoard/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const User = require('../../db/models/User')
+const {error,success} = require('../../utils/helpers/response')
+app.get('/leaderboard',async(req,res)=>{
+   try{
+    //    userId sent on payload
+        const {userId} = req.body;
+        if(userId){
+            let results = await User.leaderBoard();
+            User.getGlobalRank(userId).then(result=>{
+                res.json(success({reqId: req.id, data: {results, result}}))
+            }).catch(e=>{
+                res.status(500).json(error({reqId: req.id, code: 500, message: e}))
+            });
+
+        }else{
+            // Missing ID of userId;
+            res.status(400).json(error({reqId: req.id, code: 400, message: "Missing userId sent on payload"}))
+        }
+   }catch(e){
+    res.status(500).json(error({reqId: req.id, code: 500, message: e}))
+   }
+})
+
+module.exports = app

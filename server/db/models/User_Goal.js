952d822bffbcf548326bbe883e926b09a82d99fe
Ferm9404 <Fernando.rivas@focusservices.com>
2021-04-06 01:37:39 -0600

    Add endpoint to get user_goals per facility

--- server/db/models/User_Goal.js
+++ server/db/models/User_Goal.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Goal = require('./Goal')
 const userGoalSchema = new mongoose.Schema({
     userFacilityId:{
         type: mongoose.Schema.Types.ObjectId,
@@ -21,4 +21,39 @@ const userGoalSchema = new mongoose.Schema({
 
 })
 
-module.exports = mongoose.model('user_goal',userGoalSchema)
+userGoalSchema.statics.findAllUserGoals = async (userFacilityId)=>{
+    return new Promise((resolve,reject)=>{
+        UserGoals.aggregate([
+            {
+                "$match": {"userFacilityId": userFacilityId._id}
+            },
+            {
+                "$lookup":{
+                    "from": "goals",
+                    "localField":"goalId",
+                    "foreignField": "_id",
+                    "as": "goal",
+                }
+               
+            },
+            {
+                "$unwind": "$goal"
+                       
+            },
+            {
+                "$project":{
+                    "__v": 0,
+                    "userFacilityId": 0
+                }
+            }
+         ]).exec((err,results)=>{
+                if(err) reject(err)
+                resolve(results);
+         })
+     
+    })
+       
+}
+
+const UserGoals = mongoose.model('user_goal',userGoalSchema)
+module.exports = UserGoals
